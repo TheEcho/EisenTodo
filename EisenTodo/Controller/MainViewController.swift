e617c6c45aa063d28ddc2bddc0e400360796384a
Alex <alex@Alexs-Mac.local>
2018-04-03 22:01:38 +0200

    Retrieve/create User Data in firebase + Retrieve user linked tasks

--- EisenTodo/Controller/MainViewController.swift
+++ EisenTodo/Controller/MainViewController.swift
@@ -12,6 +12,7 @@ import Firebase
 class MainViewController: UIViewController {
 
     var authStateHandle: AuthStateDidChangeListenerHandle?
+    var userId: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +21,7 @@ class MainViewController: UIViewController {
         authStateHandle = Auth.auth().addStateDidChangeListener { (auth, user) in
 
         }
+        fetchData()
     }
 
     override func viewWillDisappear(_ animated: Bool) {
@@ -31,6 +33,71 @@ class MainViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - Data Handling (note: firebase uid is unique)
+    
+    func fetchData() {
+        let user = Auth.auth().currentUser
+
+        if let user = user {
+            let db = Firestore.firestore()
+            
+            db.collection("users").whereField("uid", isEqualTo: user.uid)
+                .getDocuments() { (querySnapshot, err) in
+                    if let err = err {
+                        print("Error getting documents: \(err)")
+                    } else if querySnapshot!.documents.count > 1 {
+                        print("Error: multiple documents with same uid")
+                    } else if querySnapshot!.documents.count < 1 {
+                        print("No document with this uid. Creating one...")
+                        self.createUserData(user: user)
+                    } else {
+                        let document = querySnapshot!.documents.first!
+                        self.userId = (document.data()["uid"] as! String)
+                        print("\(document.documentID) => \(document.data())")
+                        self.fetchUserTasks()
+                    }
+            }
+        }
+    }
+    
+    func createUserData(user: User) {
+        let db = Firestore.firestore()
+        var ref: DocumentReference? = nil
+
+        ref = db.collection("users").addDocument(data: [
+            "displayName": user.displayName as Any,
+            "photoUrl": user.photoURL?.absoluteString as Any,
+            "uid": user.uid
+        ]) { err in
+            if let err = err {
+                print("Error adding document: \(err)")
+            } else {
+                print("Document added with ID: \(ref!.documentID)")
+                self.fetchData()
+            }
+        }
+    }
+    
+    func fetchUserTasks() {
+        if let uid = self.userId {
+            let db = Firestore.firestore()
+
+            print("Retrieving documents for uid \(uid) : ")
+
+            db.collection("tasks").whereField("users.\(uid)", isEqualTo: true)
+                .getDocuments() { (querySnapshot, err) in
+                    if let err = err {
+                        print("Error getting documents: \(err)")
+                    } else {
+                        for document in querySnapshot!.documents {
+                            print("\(document.documentID) => \(document.data())")
+                            // Display tasks ...
+                        }
+                    }
+            }
+        }
+    }
+    
     // MARK: - Navigation
     @IBAction func unwindToHome(segue:UIStoryboardSegue) { }
     
